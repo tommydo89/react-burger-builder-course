2933505430021262f34d01616e2e756b73431b5b
VISTAPRINTUS\jmaffeo <jmaffeo@vistaprint.com>
2019-01-03 18:52:28 -0500

    add sidedrawer toggle, add hamburger icon, improve performance

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../../hoc/_Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-	const ingredientSummary = Object.keys(props.ingredients)
-		.map(igKey => {
-			return (
-			<li key={igKey}>
-				<span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-			</li>
-			);
-		});
-
-	return (
-		<Aux>
-			<h3>Your Order</h3>
-			<p>A delicious burger with the follow ingredients:</p>
-			<ul>
-				{ingredientSummary}
-			</ul>
-			<p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-			<p>Continue to Checkout?</p>
-			<Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
-			<Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-		</Aux>
-	)
+class OrderSummary extends Component {
+	//this can be a functional component, doesn't have to be a class
+	// componentWillUpdate() {
+	// 	console.log('OrderSummary will update');
+	// }
+	render (){
+		const ingredientSummary = Object.keys(this.props.ingredients)
+			.map(igKey => {
+				return (
+				<li key={igKey}>
+					<span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+				</li>
+				);
+			});
+		return (
+			<Aux>
+				<h3>Your Order</h3>
+				<p>A delicious burger with the follow ingredients:</p>
+				<ul>
+					{ingredientSummary}
+				</ul>
+				<p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+				<p>Continue to Checkout?</p>
+				<Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+				<Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+			</Aux>
+		)
+	}
 };
 
-export default orderSummary;
+export default OrderSummary;
