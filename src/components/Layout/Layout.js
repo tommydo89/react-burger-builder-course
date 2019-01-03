2403703adb8d626c97eb93b30a6ed774a07a9dfd
Tommy Do <tommy.td89@gmail.com>
2019-01-02 22:02:02 -0500

    init commit

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import Aux from '../../hoc/_Aux';
+import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+
+class Layout extends Component {
+
+	state = {
+		showSideDrawer: true
+	}
+
+	sideDrawerClosedHandler = () => {
+		this.setState({showSideDrawer: false});
+	}
+
+	render() {
+		return (
+			<Aux>
+				<Toolbar />
+				<SideDrawer 
+					open={this.state.showSideDrawer}
+					closed={this.sideDrawerClosedHandler}/>
+				<main className={classes.Content}>
+					{this.props.children}
+				</main>
+			</Aux>
+		);
+	}
+}
+
+export default Layout;
